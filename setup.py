c802ab2916f47fed5dd43e1e1d1fe35d883af812
fetene <bfetene@gmx.net>
2026-06-19 16:27:07 +0200

    requirments added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    verion="0.1.0",
+    author=" Boktior Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
